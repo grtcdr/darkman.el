0d0546d15b7f39fcf07a278fdd60733db6a3666c
grtcdr <tahaaziz.benali@esprit.tn>
2023-01-11 04:53:12 +0100

    Store D-Bus signal

--- darkman.el
+++ darkman.el
@@ -3,34 +3,39 @@
 ;; Copyright (C) 2023  Aziz Ben Ali
 
 ;; Author: Aziz Ben Ali <tahaaziz.benali@esprit.tn>
+;; Homepage: http://github.com/grtcdr/darkman.el
+;; Version: 0.1
 ;; Keywords: convenience
 
 (require 'dbus)
 
+(defvar darkman-themes '(:light modus-operandi :dark modus-vivendi))
 (defvar darkman--dbus-service "nl.whynothugo.darkman")
 (defvar darkman--dbus-path (car (last (dbus-introspect-get-all-nodes :session darkman--dbus-service "/"))))
 (defvar darkman--dbus-interface darkman--dbus-service)
-(defvar darkman-themes '(:light modus-operandi :dark modus-vivendi))
+(defvar darkman--dbus-signal nil)
 
 (defun darkman--invalid-mode-error (mode)
-    "Signal an error about an invalid mode. MODE is the name of the invalid mode."
-    (error (format "‘%s’ is not a valid mode." mode)))
+  "Signal an error about an invalid mode. MODE is the name of the invalid mode."
+  (error (format "‘%s’ is not a valid mode." mode)))
 
 (defun darkman--dbus-service-unavailable-error ()
   "Signal an error about an invalid mode. MODE is the name of the invalid mode."
   (error (format "%s D-Bus service not available."
 		 darkman--dbus-service)))
 
-(defun darkman-get-mode ()
+(defun darkman-get ()
   "Get the mode of the darkman service."
   (interactive)
-  (dbus-get-property :session
+  (let ((mode (dbus-get-property :session
 		     darkman--dbus-service
 		     darkman--dbus-path
 		     darkman--dbus-interface
-		     "Mode"))
+		     "Mode")))
+    (when (called-interactively-p) (message (format "Mode is currently set to %s." mode)))
+    mode))
 
-(defun darkman-set-mode (mode)
+(defun darkman-set (mode)
   "Set the mode of the darkman service to MODE. MODE can be ‘dark’
 or ‘light’."
   (dbus-set-property :session
@@ -41,12 +46,12 @@ or ‘light’."
 				   (symbol-name mode))
 				  (t (darkman--invalid-mode-error mode)))))
 
-(defun darkman-toggle ()
+(defun darkman-toggle-mode-of-service ()
   "Toggle the mode of the darkman service."
   (interactive)
-  (let ((mode (darkman-get-mode)))
-    (cond ((string= mode "dark") (darkman-set-mode 'light))
-	  ((string= mode "light") (darkman-set-mode 'dark)))))
+  (let ((mode (darkman-get)))
+    (cond ((string= mode "dark") (darkman-set 'light))
+	  ((string= mode "light") (darkman-set 'dark)))))
 
 (defun darkman--get-assoc-theme (mode)
   "Return a theme from ‘darkman-themes’ which corresponds to THEME."
@@ -56,7 +61,7 @@ or ‘light’."
 
 (defun darkman-get-theme ()
   "Get a theme from the ‘darkman-themes’ which corresponds to the current mode."
-  (let ((mode (darkman-get-mode)))
+  (let ((mode (darkman-get)))
     (darkman--get-assoc-theme mode)))
 
 (defun darkman--mode-changed-signal-handler (new-mode)
@@ -75,13 +80,16 @@ or ‘light’."
   :init-value nil
   :require 'dbus
   :version "0.1"
-  (and (darkman--check-dbus-service)
-       (dbus-register-signal :system
-			     darkman--dbus-service
-			     darkman--dbus-path
-			     darkman--dbus-interface
-			     "ModeChanged"
-			     #'darkman--mode-changed-signal-handler)))
+  (if darkman-mode
+      (and (darkman--check-dbus-service)
+	   (setq darkman--dbus-signal
+		 (dbus-register-signal :system
+				       darkman--dbus-service
+				       darkman--dbus-path
+				       darkman--dbus-interface
+				       "ModeChanged"
+				       #'darkman--mode-changed-signal-handler)))
+    (setq darkman--dbus-signal nil)))
 
 (provide 'darkman)
 ;; darkman.el ends here
