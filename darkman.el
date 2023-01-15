ebb5d3552abb39c577cb391db359452a69d3c1f7
grtcdr <tahaaziz.benali@esprit.tn>
2023-01-15 04:36:32 +0100

    Conform to code conventions
    And use consistent casing throughout docstrings.

--- darkman.el
+++ darkman.el
@@ -7,7 +7,33 @@
 ;; Version: 0.1
 ;; Keywords: convenience
 
+;; This file is not part of GNU Emacs.
+
+;;; License:
+;;
+;; Permission to use, copy, modify, and/or distribute this software for
+;; any purpose with or without fee is hereby granted, provided that the
+;; above copyright notice and this permission notice appear in all
+;; copies.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+;; WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+;; AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+;; DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+;; PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+;; TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+;; PERFORMANCE OF THIS SOFTWARE.
+
+
+;;; Commentary:
+;;
+;; darkman.el provides seamless integration between Darkman and Emacs
+;; using the D-Bus protocol.
+
 (require 'dbus)
+
+;;; Code:
 
 (defgroup darkman ()
   "Seamless integration between Darkman and Emacs."
@@ -36,7 +62,7 @@ when the mode is changed."
 (defvar darkman--dbus-signal nil)
 
 (defun darkman-get ()
-  "Get the mode of the darkman service."
+  "Get the mode of the Darkman service."
   (interactive)
   (let ((mode (dbus-get-property :session
 				 darkman--dbus-service
@@ -49,7 +75,7 @@ when the mode is changed."
 
 ;;;###autoload
 (defun darkman-set (mode)
-  "Set the mode of the darkman service to MODE, which can either be
+  "Set the mode of the Darkman service to MODE, which can either be
 ‘dark’ or ‘light’."
   (dbus-set-property :session
 		     darkman--dbus-service
@@ -60,18 +86,18 @@ when the mode is changed."
 				  (t (darkman--invalid-mode-error mode)))))
 
 (defun darkman-toggle ()
-  "Toggle the mode of the darkman service."
+  "Toggle the mode of the Darkman service."
   (interactive)
   (let ((mode (darkman-get)))
     (cond ((string= mode "dark") (darkman-set 'light))
 	  ((string= mode "light") (darkman-set 'dark)))))
 
 (defun darkman--invalid-mode-error (mode)
-  "Signal an error about an invalid mode. MODE is the name of the invalid mode."
+  "Signal an error about an invalid mode.  MODE is the name of the invalid mode."
   (error (format "‘%s’ is not a valid mode." mode)))
 
 (defun darkman--dbus-service-unavailable-error ()
-  "Signal an error about an invalid mode. MODE is the name of the invalid mode."
+  "Signal an error about an invalid mode.  MODE is the name of the invalid mode."
   (error (format "%s D-Bus service not available."
 		 darkman--dbus-service)))
 
@@ -88,7 +114,7 @@ when the mode is changed."
     (darkman--get-assoc-theme mode)))
 
 (defun darkman--mode-changed-signal-handler (new-mode)
-  "Signal handler for the ModeChanged signal."
+  "Signal handler for the ModeChanged signal.  NEW-MODE is the new mode."
   (let ((new-theme (darkman--get-assoc-theme new-mode)))
     (unless (not darkman-switch-themes-silently)
       (message (format "Darkman switched to %s mode, switching to %s theme."
@@ -96,12 +122,12 @@ when the mode is changed."
     (load-theme new-theme)))
 
 (defun darkman--check-dbus-service ()
-  "Return non-nil if the darkman service is available."
+  "Return non-nil if the Darkman service is available."
   (or (dbus-ping :session darkman--dbus-service 100)
       (darkman--dbus-service-unavailable-error)))
 
 (define-minor-mode darkman-mode
-  "Minor mode providing integration with the darkman utility."
+  "Minor mode providing integration with the Darkman utility."
   :global t
   :init-value nil
   :require 'dbus
