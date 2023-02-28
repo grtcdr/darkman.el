9156a8c4e943d9fb59e38ff1e3fce7f8ac703609
Agustín Cisneros <agustincc@tutanota.com>
2023-02-28 13:59:49 +0100

    Remove indirection from handler function
    Too early and few cases to do so.

--- darkman.el
+++ darkman.el
@@ -110,25 +110,20 @@ when the mode is changed."
   "Get a theme from the ‘darkman-themes’ which corresponds to the current mode."
   (darkman--lookup-theme (darkman-get)))
 
-(defun darkman-load-theme-mode (new-mode)
-  "Load a theme associated with the NEW-MODE."
-  (let ((new-theme (darkman--lookup-theme new-mode)))
-    (unless darkman-switch-themes-silently
-      (message (format "Darkman switched to %s mode, switching to %s theme."
-                       new-mode new-theme)))
-    (load-theme new-theme)))
-
 (defun darkman--call-event-handler (interface property value)
   "Handle method_call events on Darkman DBus.
 
 INTERFACE is the name of the interface that is the target of the event.
-
 PROPERTY is the property that is modified by the event.
-
 VALUE is the new value of PROPERTY."
   (when (and (string-equal "Mode" property)
              (equal darkman--dbus-service interface))
-    (darkman-load-theme-mode (car value))))
+    (let* ((new-mode (car value))
+           (new-theme (darkman--lookup-theme new-mode)))
+      (unless darkman-switch-themes-silently
+        (message (format "Darkman switched to %s mode, switching to %s theme."
+                         new-mode new-theme)))
+      (load-theme new-theme))))
 
 (defun darkman--check-dbus-service ()
   "Return non-nil if the Darkman service is available."
