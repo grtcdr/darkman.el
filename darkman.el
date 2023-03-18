9f1f713b210ab7317191ab5c73e8e4216d0ef8ab
grtcdr <tahaaziz.benali@esprit.tn>
2023-03-18 11:35:38 +0100

    Bump version to 1.0.2

--- darkman.el
+++ darkman.el
@@ -5,7 +5,7 @@
 ;; Author: Aziz Ben Ali <tahaaziz.benali@esprit.tn>
 ;; Maintainer: Aziz Ben Ali <tahaaziz.benali@esprit.tn>
 ;; Homepage: https://grtcdr.tn/darkman.el/
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience
 
