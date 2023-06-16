fb9dd1858f6503cc3478ef0bfa6750165d5c5cbc
grtcdr <tahaaziz.benali@esprit.tn>
2023-06-16 21:22:42 +0100

    Add unit test for darkman--lookup-mode function

--- darkman-test.el
+++ darkman-test.el
@@ -34,9 +34,21 @@
 (require 'ert)
 (require 'darkman)
 
+(defmacro darkman-with-themes (&rest body)
+  "Evaluate BODY with a predefined ‘darkman-themes’ value."
+  (declare (indent nil))
+  `(let ((darkman-themes '(:light tango :dark tango-dark)))
+     ,@body))
+
 (ert-deftest darkman-test-lookup-theme ()
   "Test the return value of ‘darkman--lookup-theme’."
-  (let ((darkman-themes '(:light tango :dark tango-dark)))
-    (should (equal (darkman--lookup-theme "dark") 'tango-dark))
-    (should (equal (darkman--lookup-theme "light") 'tango))
-    (should (equal (darkman--lookup-theme "dim") nil))))
+  (darkman-with-themes
+   (should (equal (darkman--lookup-theme "dark") 'tango-dark))
+   (should (equal (darkman--lookup-theme "light") 'tango))
+   (should (equal (darkman--lookup-theme "dim") nil))))
+
+(ert-deftest darkman-test-lookup-mode ()
+  (darkman-with-themes
+   (should (string-equal (darkman--lookup-mode "tango") "light"))
+   (should (string-equal (darkman--lookup-mode "tango-dark") "dark"))
+   (should (equal (darkman--lookup-mode "modus-vivendi") nil))))
